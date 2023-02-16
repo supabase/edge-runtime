dded1edc2cacc74fabaacebcfaefb538f1c1b18e
Lakshan Perera <lakshan@laktek.com>
2023-02-17 07:14:43 +1100

    fix: import defineEvent from deno web module

--- base/src/js_worker/js/bootstrap.js
+++ base/src/js_worker/js/bootstrap.js
@@ -21,6 +21,7 @@
   const encoding = window.__bootstrap.encoding;
   //const errors = window.__bootstrap.errors.errors;
   const event = window.__bootstrap.event;
+  const { defineEventHandler } = window.__bootstrap.event;
   const eventTarget = window.__bootstrap.eventTarget;
   const { env } = window.__bootstrap.os;
   const fetch = window.__bootstrap.fetch;
@@ -123,57 +124,6 @@
       enumerable: true,
       configurable: true,
     };
-  }
-
-  // TODO move defineEventHandler to a separate file
-
-  // `init` is an optional function that will be called the first time that the
-  // event handler property is set. It will be called with the object on which
-  // the property is set as its argument.
-  // `isSpecialErrorEventHandler` can be set to true to opt into the special
-  // behavior of event handlers for the "error" event in a global scope.
-  function defineEventHandler(
-    emitter,
-    name,
-    init = undefined,
-    isSpecialErrorEventHandler = false,
-  ) {
-    // HTML specification section 8.1.7.1
-    ObjectDefineProperty(emitter, `on${name}`, {
-      get() {
-        if (!this[_eventHandlers]) {
-          return null;
-        }
-
-        return MapPrototypeGet(this[_eventHandlers], name)?.handler ?? null;
-      },
-      set(value) {
-        // All three Web IDL event handler types are nullable callback functions
-        // with the [LegacyTreatNonObjectAsNull] extended attribute, meaning
-        // anything other than an object is treated as null.
-        if (typeof value !== "object" && typeof value !== "function") {
-          value = null;
-        }
-
-        if (!this[_eventHandlers]) {
-          this[_eventHandlers] = new Map();
-        }
-        let handlerWrapper = MapPrototypeGet(this[_eventHandlers], name);
-        if (handlerWrapper) {
-          handlerWrapper.handler = value;
-        } else if (value !== null) {
-          handlerWrapper = makeWrappedHandler(
-            value,
-            isSpecialErrorEventHandler,
-          );
-          this.addEventListener(name, handlerWrapper);
-          init?.(this);
-        }
-        MapPrototypeSet(this[_eventHandlers], name, handlerWrapper);
-      },
-      configurable: true,
-      enumerable: true,
-    });
   }
 
   const globalScope = {

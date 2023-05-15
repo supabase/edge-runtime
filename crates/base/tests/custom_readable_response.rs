84aecde8c815fd33490b00d08c9c4cd154511917
andreespirela <andreespirela@outlook.com>
2023-05-15 19:57:01 -0400

    Moving test to its own file

---
+++ crates/base/tests/custom_readable_response.rs
@@ -0,0 +1,20 @@
+use tokio::select;
+use base::commands::start_server;
+
+#[tokio::test]
+async fn test_custom_readable_stream_response() {
+    select! {
+        _ = start_server(
+            "0.0.0.0",
+            8999,
+            String::from("./test_cases/main"),
+            None,
+            false
+        ) => {
+            panic!("This one should not end first");
+        }
+        resp = reqwest::get("http://localhost:8999/readable-stream-resp") => {
+            assert_eq!(resp.unwrap().text().await.unwrap(), "Hello world from streams");
+        }
+    }
+}
--- crates/base/tests/null_body_status_null_body_tests.rs
+++ crates/base/tests/null_body_status_null_body_tests.rs
@@ -71,21 +71,3 @@ async fn test_null_body_with_204_status_post() {
 
     assert_eq!(body_bytes.len(), 0);
 }
-
-#[tokio::test]
-async fn test_custom_readable_stream_response() {
-    select! {
-        _ = start_server(
-            "0.0.0.0",
-            8999,
-            String::from("./test_cases/main"),
-            None,
-            false
-        ) => {
-            panic!("This one should not end first");
-        }
-        resp = reqwest::get("http://localhost:8999/readable-stream-resp") => {
-            assert_eq!(resp.unwrap().text().await.unwrap(), "Hello world from streams");
-        }
-    }
-}

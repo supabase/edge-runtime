52a9b0287d5e22c3099f550e6160c8e74ba854e8
andreespirela <andreespirela@outlook.com>
2023-05-15 20:56:40 -0400

    add sleeping test

--- crates/base/tests/custom_readable_response.rs
+++ crates/base/tests/custom_readable_response.rs
@@ -1,8 +1,16 @@
+use std::thread::sleep;
+use std::time::Duration;
 use base::commands::start_server;
 use tokio::select;
 
 #[tokio::test]
 async fn test_custom_readable_stream_response() {
+
+    let req = async {
+        sleep(Duration::from_secs(3));
+        reqwest::get("http://localhost:8999/readable-stream-resp").await
+    };
+
     select! {
         _ = start_server(
             "0.0.0.0",
@@ -13,7 +21,7 @@ async fn test_custom_readable_stream_response() {
         ) => {
             panic!("This one should not end first");
         }
-        resp = reqwest::get("http://localhost:8999/readable-stream-resp") => {
+        resp = req => {
             assert_eq!(resp.unwrap().text().await.unwrap(), "Hello world from streams");
         }
     }

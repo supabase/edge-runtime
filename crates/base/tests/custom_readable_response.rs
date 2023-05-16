350a6e25474af43b2e63ccb7786adfefc8a2f7db
andreespirela <andreespirela@outlook.com>
2023-05-15 21:00:14 -0400

    fmt

--- crates/base/tests/custom_readable_response.rs
+++ crates/base/tests/custom_readable_response.rs
@@ -1,11 +1,10 @@
+use base::commands::start_server;
 use std::thread::sleep;
 use std::time::Duration;
-use base::commands::start_server;
 use tokio::select;
 
 #[tokio::test]
 async fn test_custom_readable_stream_response() {
-
     let req = async {
         sleep(Duration::from_secs(3));
         reqwest::get("http://localhost:8999/readable-stream-resp").await

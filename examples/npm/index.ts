0439b82d555dfdbb58cee477d3f6dda096f7dd40
andreespirela <andreespirela@outlook.com>
2023-11-14 11:55:38 -0500

    chore: Change text to supabase

--- examples/npm/index.ts
+++ examples/npm/index.ts
@@ -3,7 +3,7 @@ import express from "npm:express@4.18.2";
 const app = express();
 
 app.get(/(.*)/, (req, res) => {
-    res.send("Welcome to the Dinosaur API!");
+    res.send("Welcome to Supabase");
 });
 
 app.listen(8000);

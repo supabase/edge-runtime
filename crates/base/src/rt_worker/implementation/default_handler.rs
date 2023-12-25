91b9d0ebf0b2725639dcecae67058e0623d3362a
Nyannyacha <meow@nnc.gg>
2023-12-25 15:24:33 +0000

    fix: logging unexpected event loop escape explicitly
    (cherry picked from commit d10bc64d0aef6ca9e8ada95b8be850a7ae052a97)

--- crates/base/src/rt_worker/implementation/default_handler.rs
+++ crates/base/src/rt_worker/implementation/default_handler.rs
@@ -2,6 +2,7 @@ use crate::deno_runtime::DenoRuntime;
 use crate::rt_worker::worker::{HandleCreationType, Worker, WorkerHandler};
 use anyhow::Error;
 use event_worker::events::{BootFailureEvent, PseudoEvent, UncaughtExceptionEvent, WorkerEvents};
+use log::error;
 use sb_core::conn_sync::ConnSync;
 use std::any::Any;
 use tokio::net::UnixStream;
@@ -28,9 +29,15 @@ impl WorkerHandler for Worker {
                 // if the error is execution terminated, check termination event reason
                 Err(err) => {
                     let err_string = err.to_string();
+
                     if err_string.ends_with("execution terminated") {
                         Ok(termination_event_rx.await.unwrap())
                     } else {
+                        error!(
+                            "runtime has escaped from the event loop unexpectedly: {}",
+                            err_string.as_str()
+                        );
+
                         Ok(WorkerEvents::UncaughtException(UncaughtExceptionEvent {
                             exception: err_string,
                             cpu_time_used: 0, // this will be set later

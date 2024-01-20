151004ce32ebc862c2a9037356146919583fe3c0
Nyannyacha <meow@nnc.gg>
2024-01-20 14:23:11 +0000

    fix: make major worker tasks managed separately from the user worker
    (cherry picked from commit a80bf002b5d9e47c9ebc3c34f3e90155baeb07a4)

--- crates/base/src/rt_worker/rt.rs
+++ crates/base/src/rt_worker/rt.rs
@@ -8,7 +8,13 @@ pub static SUPERVISOR_RT: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
         .unwrap()
 });
 
-pub static WORKER_RT: Lazy<tokio_util::task::LocalPoolHandle> = Lazy::new(|| {
+// NOTE: This pool is for the main and event workers. The reason why they should
+// separate from the user worker pool is they can starve them if user workers
+// are saturated.
+pub static PRIMARY_WORKER_RT: Lazy<tokio_util::task::LocalPoolHandle> =
+    Lazy::new(|| tokio_util::task::LocalPoolHandle::new(2));
+
+pub static USER_WORKER_RT: Lazy<tokio_util::task::LocalPoolHandle> = Lazy::new(|| {
     let maybe_pool_size = std::env::var("EDGE_RUNTIME_WORKER_POOL_SIZE")
         .ok()
         .and_then(|it| it.parse::<usize>().ok());

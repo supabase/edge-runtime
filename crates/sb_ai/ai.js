854245d90e382b96461ee07790fff572166a2bc6
Lakshan Perera <lakshan@laktek.com>
2024-04-04 19:10:50 +1100

    wip

--- crates/sb_ai/ai.js
+++ crates/sb_ai/ai.js
@@ -2,13 +2,29 @@ const core = globalThis.Deno.core;
 
 class Session {
 	model;
+	is_ollama;
 
 	constructor(model) {
 		this.model = model;
-		core.ops.op_sb_ai_init_model(model);
+		this.is_ollama = false;
+
+		if (model === 'llama2' || model === 'mixtral' || model === 'mistral') {
+			this.is_ollama = true;
+		} else {
+			core.ops.op_sb_ai_init_model(model);
+		}
 	}
 
 	async run(prompt, opts = {}) {
+		if (this.is_ollama) {
+			const res = await fetch('https://lakshan-ollama.fly.dev/api/generate', {
+				body: JSON.stringify({ model: this.model, prompt, stream: false }),
+				method: 'POST',
+			});
+			const json = await res.json();
+			return json.response;
+		}
+
 		const mean_pool = opts.mean_pool ?? true;
 		const normalize = opts.normalize ?? true;
 		const result = await core.ops.op_sb_ai_run_model(this.model, prompt, mean_pool, normalize);
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GIT_V_TAG=0.1.1 cargo build && EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start --main-service ./examples/main --event-worker ./examples/event-manager
+GIT_V_TAG=0.1.1 cargo build && ORT_DYLIB_PATH=/Users/lakshanperera/workspace/edge-runtime/libonnxruntime.so.1.17.0 SB_AI_MODELS_DIR=/Users/lakshanperera/workspace/edge-runtime/models EDGE_RUNTIME_PORT=9000 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start --main-service ./examples/main --event-worker ./examples/event-manager

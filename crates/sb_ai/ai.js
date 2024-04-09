a924a4d9ae1c4ac0f3228238d97f7fddfd9cc34a
Lakshan Perera <lakshan@laktek.com>
2024-04-10 05:32:42 +1000

    support streaming responses

--- crates/sb_ai/ai.js
+++ crates/sb_ai/ai.js
@@ -1,5 +1,42 @@
 const core = globalThis.Deno.core;
 
+const parseJSON = async function* (itr) {
+	const decoder = new TextDecoder('utf-8');
+	let buffer = '';
+
+	const reader = itr.getReader();
+
+	while (true) {
+		const { done, value: chunk } = await reader.read();
+
+		if (done) {
+			break;
+		}
+
+		buffer += decoder.decode(chunk);
+
+		const parts = buffer.split('\n');
+
+		buffer = parts.pop() ?? '';
+
+		for (const part of parts) {
+			try {
+				yield JSON.parse(part);
+			} catch (error) {
+				console.warn('invalid json: ', part);
+			}
+		}
+	}
+
+	for (const part of buffer.split('\n').filter((p) => p !== '')) {
+		try {
+			yield JSON.parse(part);
+		} catch (error) {
+			console.warn('invalid json: ', part);
+		}
+	}
+};
+
 class Session {
 	model;
 	is_ollama;
@@ -17,12 +54,45 @@ class Session {
 
 	async run(prompt, opts = {}) {
 		if (this.is_ollama) {
-			const res = await fetch('https://lakshan-ollama.fly.dev/api/generate', {
-				body: JSON.stringify({ model: this.model, prompt, stream: false }),
+			const inferenceAPIHost = core.ops.op_get_env('AI_INFERENCE_API_HOST');
+			const stream = opts.stream ?? false;
+			const timeout = opts.timeout ?? 60; // default timeout 60s
+
+			const controller = new AbortController();
+			const signal = controller.signal;
+			setTimeout(() => controller.abort(new Error('Request timeout')), 30 * 1000);
+
+			const res = await fetch(`${inferenceAPIHost}/api/generate`, {
+				body: JSON.stringify({ model: this.model, prompt, stream }),
 				method: 'POST',
-			});
-			const json = await res.json();
-			return json.response;
+			}, { signal });
+
+			if (!res.body) {
+				throw new Error('Missing body');
+			}
+
+			const itr = parseJSON(res.body);
+
+			if (stream) {
+				return (async function* () {
+					for await (const message of itr) {
+						if ('error' in message) {
+							throw new Error(message.error);
+						}
+						yield message;
+						if (message.done) {
+							return;
+						}
+					}
+					throw new Error('Did not receive done or success response in stream.');
+				})();
+			} else {
+				const message = await itr.next();
+				if (!message.value.done && message.value.status !== 'success') {
+					throw new Error('Expected a completed response.');
+				}
+				return message.value;
+			}
 		}
 
 		const mean_pool = opts.mean_pool ?? true;

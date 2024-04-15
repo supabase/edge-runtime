52793e2c1744761482a249465aefbcdba182760f
Nyannyacha <meow@nnc.gg>
2024-04-15 01:34:15 +0000

    stamp: update `run.sh`

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GIT_V_TAG=0.1.1 cargo build && EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start --main-service ./examples/main --event-worker ./examples/event-manager
+GIT_V_TAG=0.1.1 cargo build && EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start --main-service ./examples/main --event-worker ./examples/event-manager --static "./examples/**/*.json"

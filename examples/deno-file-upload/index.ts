9f0228dba9e829e52164d863144dc80373fa28cb
Nyannyacha <meow@nnc.gg>
2024-04-25 03:19:13 +0000

    stamp: update deno file update example

--- examples/deno-file-upload/index.ts
+++ examples/deno-file-upload/index.ts
@@ -1,4 +1,10 @@
 Deno.serve(async (req: Request) => {
+    if (req.method === "GET" || req.method === "HEAD") {
+        // NOTE: https://github.com/whatwg/fetch/issues/1705
+        console.log(req.body); // null
+        return new Response("meow?");
+    }
+
     // NOTE: https://github.com/whatwg/fetch/issues/1592
     const form = await req.formData();
     const metadata = {};

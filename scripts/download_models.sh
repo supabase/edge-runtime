2902bc96aab209f6b6e00db5669e573339f34906
kallebysantos <kalleby_santos@hotmail.com>
2024-05-22 11:37:34 +0100

    feat: Downloading custom models
    - Added Model Name support for `download_models.sh` script.
    ### Usage:
    ```bash
    $ download_models.sh "<HuggingFace model name | Default:
    Supabase/gte-small >"
    ```
    .1 Donwloading a custom model
    [`Xenova/paraphrase-multilingual-MiniLM-L12-v2`](https://huggingface.co/Xenova/paraphrase-multilingual-MiniLM-L12-v2)
    ```bash
    $ download_models.sh "Xenova/paraphrase-multilingual-MiniLM-L12-v2"
    ```
    
    .2 Donwloading default `gte-small`
    ```bash
    $ download_models.sh
    ```

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p models/gte-small
-cd models/gte-small
-curl -L -o model.onnx https://huggingface.co/Supabase/gte-small/resolve/main/onnx/model.onnx?download=true
-curl -L -o tokenizer.json https://huggingface.co/Supabase/gte-small/resolve/main/tokenizer.json?download=true
+MODEL_NAME=${1:-Supabase/gte-small}
+MODEL_PATH="models/$(awk -F'/' '{print $2}' <<<$MODEL_NAME)"
+
+mkdir -p $MODEL_PATH
+
+curl -L -o $MODEL_PATH/model.onnx https://huggingface.co/$MODEL_NAME/resolve/main/onnx/model.onnx?download=true
+curl -L -o $MODEL_PATH/tokenizer.json https://huggingface.co/$MODEL_NAME/resolve/main/tokenizer.json?download=true

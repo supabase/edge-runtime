eaea14d0a0c313fb22a6af22b0ee25d44528589d
Nyannyacha <meow@nnc.gg>
2024-06-18 00:02:51 +0000

    stamp(base): move test contents into each test directory

--- crates/base/test_cases/read_file_sync_20mib/index.ts
+++ crates/base/test_cases/read_file_sync_20mib/index.ts
@@ -1,5 +1,5 @@
 // NOTE: Just defined to prevent the JsRuntime leave from the event loop
 Deno.serve(() => { /* do nothing */ });
 
-let buf_in_ext_mem = Deno.readFileSync("../meow_20mib.bin") as Uint8Array;
+let buf_in_ext_mem = Deno.readFileSync("20mib.bin") as Uint8Array;
 console.log(buf_in_ext_mem.length); // to prevent optimization

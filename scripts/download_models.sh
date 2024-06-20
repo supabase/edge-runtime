73eb3a490e24a2bda3b1cdc0c751ee846735c70f
kallebysantos <kalleby_santos@hotmail.com>
2024-06-20 17:09:05 +0100

    feat: support for quantized model download
    Added `--quantized` and `--f16` flags to `download-models.sh` script

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -7,7 +7,18 @@ MODEL_PATH="models/$(awk -F'/' '{print $2}' <<<$MODEL_NAME)"
 
 mkdir -p $MODEL_PATH
 
-curl -L -o $MODEL_PATH/model.onnx https://huggingface.co/$MODEL_NAME/resolve/main/onnx/model.onnx?download=true
+MODEL_FILENAME="model.onnx"
+
+# Downloads the float 16 version
+if [[ $* == *"--f16"* ]]; then
+	MODEL_FILENAME="model_fp16.onnx"
+
+# Downloads the quantized version
+elif [[ $* == *"--quantized"* ]]; then
+	MODEL_FILENAME="model_quantized.onnx"
+fi
+
+curl -L -o $MODEL_PATH/model.onnx https://huggingface.co/$MODEL_NAME/resolve/main/onnx/$MODEL_FILENAME?download=true
 curl -L -o $MODEL_PATH/tokenizer.json https://huggingface.co/$MODEL_NAME/resolve/main/tokenizer.json?download=true
 
 # Creates symbolic link to default folder

5eef3305ca6edc79c47246822dca2d672660d481
Nyannyacha <meow@nnc.gg>
2024-07-08 05:17:32 +0000

    chore: update supabase ai example

--- examples/supa-ai/index.ts
+++ examples/supa-ai/index.ts
@@ -1,22 +1,30 @@
 const session = new Supabase.ai.Session('llama2');
 
 Deno.serve(async (req: Request) => {
-	const params = new URL(req.url).searchParams;
-	const prompt = params.get('prompt');
-	const output = await session.run(prompt, { stream: true });
+  const params = new URL(req.url).searchParams;
+  const prompt = params.get('prompt');
+  const output = await session.run(prompt, { stream: true }) as Iterator<any>;
+  const body = new ReadableStream({
+    async pull(ctrl) {
+      const item = await output.next();
 
-	let response = '';
-	for await (const part of output) {
-		response += part.response;
-	}
-	return new Response(
-		JSON.stringify(
-			response,
-		),
-		{
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		},
-	);
+      if (item.done) {
+        ctrl.close();
+        return;
+      }
+
+      ctrl.enqueue("data: ");
+      ctrl.enqueue(JSON.stringify(item.value));
+      ctrl.enqueue("\r\n\r\n");
+    }
+  });
+
+  return new Response(
+    body.pipeThrough(new TextEncoderStream()),
+    {
+      headers: {
+        'Content-Type': 'text/event-stream',
+      },
+    },
+  );
 });

b83bc8b3bec24a3929c6d05aab034dce553f49c8
Nyannyacha <meow@nnc.gg>
2024-07-08 05:19:15 +0000

    stamp: update `run.sh` (should be reverted)

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 # --features cli/tracing
 GIT_V_TAG=0.1.1 cargo build --features cli/tracing && \
+AI_INFERENCE_API_HOST=http://localhost:11434 \
 EDGE_RUNTIME_PORT=9998 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start \
     --main-service ./examples/main \
     --event-worker ./examples/event-manager

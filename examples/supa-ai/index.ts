64d520ab7a9ab8c6d76cf0dc1a15efce6adbfa28
Nyannyacha <meow@nnc.gg>
2024-07-08 07:32:31 +0000

    chore: update supabase ai example

--- examples/supa-ai/index.ts
+++ examples/supa-ai/index.ts
@@ -3,28 +3,57 @@ const session = new Supabase.ai.Session('llama2');
 Deno.serve(async (req: Request) => {
   const params = new URL(req.url).searchParams;
   const prompt = params.get('prompt');
-  const output = await session.run(prompt, { stream: true }) as Iterator<any>;
-  const body = new ReadableStream({
-    async pull(ctrl) {
-      const item = await output.next();
+  const stream = params.get('stream') === 'true' ?? false;
 
-      if (item.done) {
-        ctrl.close();
-        return;
-      }
+  let mode = params.get("mode") ?? 'ollama';
 
-      ctrl.enqueue("data: ");
-      ctrl.enqueue(JSON.stringify(item.value));
-      ctrl.enqueue("\r\n\r\n");
+  switch (mode) {
+    case 'ollama':
+    case 'openaicompatible':
+      break;
+
+    default: {
+      mode = 'ollama';
     }
-  });
+  }
 
-  return new Response(
-    body.pipeThrough(new TextEncoderStream()),
-    {
-      headers: {
-        'Content-Type': 'text/event-stream',
+  if (stream) {
+    const output = await session.run(prompt, {
+      stream,
+      mode
+    }) as Iterator<any>;
+
+    const body = new ReadableStream({
+      async pull(ctrl) {
+        const item = await output.next();
+
+        if (item.done) {
+          console.log("done!");
+          ctrl.close();
+          return;
+        }
+
+        ctrl.enqueue("data: ");
+        ctrl.enqueue(JSON.stringify(item.value));
+        ctrl.enqueue("\r\n\r\n");
+      }
+    });
+
+    return new Response(
+      body.pipeThrough(new TextEncoderStream()),
+      {
+        headers: {
+          'Content-Type': 'text/event-stream',
+        },
       },
-    },
-  );
+    );
+  } else {
+    const output = await session.run(prompt, {
+      stream,
+      mode
+    });
+
+    console.log("done!");
+    return Response.json(output);
+  }
 });

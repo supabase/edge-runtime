2554fdea869fdd600289e6cc51d4802f6564e15a
Nyannyacha <meow@nnc.gg>
2024-07-09 02:42:24 +0000

    chore: update supabase ai example

--- examples/supa-ai/index.ts
+++ examples/supa-ai/index.ts
@@ -1,9 +1,23 @@
+// @ts-ignore
+import { STATUS_CODE } from 'https://deno.land/std/http/status.ts';
+
 const session = new Supabase.ai.Session('llama2');
 
 Deno.serve(async (req: Request) => {
   const params = new URL(req.url).searchParams;
   const prompt = params.get('prompt');
   const stream = params.get('stream') === 'true' ?? false;
+  const controller = new AbortController();
+  const signal = AbortSignal.any([
+    // req.signal,
+    // NOTE(Nyannyacha): Abort signal in the request phase doesn't yet support.
+    //
+    // Backgrounds:
+    // [1]: https://github.com/denoland/deno/issues/21653#issuecomment-2096544782
+    // [2]: https://github.com/denoland/deno/issues/21590#issuecomment-2096544187
+    // [3]: https://github.com/denoland/deno/pull/23425
+    controller.signal
+  ]);
 
   let mode = params.get("mode") ?? 'ollama';
 
@@ -20,22 +34,38 @@ Deno.serve(async (req: Request) => {
   if (stream) {
     const output = await session.run(prompt, {
       stream,
-      mode
-    }) as Iterator<any>;
+      mode,
+      signal,
+    }) as AsyncGenerator<any>;
 
     const body = new ReadableStream({
       async pull(ctrl) {
-        const item = await output.next();
+        try {
+          const item = await output.next();
 
-        if (item.done) {
-          console.log("done!");
+          if (item.done) {
+            console.log("done");
+            ctrl.close();
+            return;
+          }
+
+          ctrl.enqueue("data: ");
+          ctrl.enqueue(JSON.stringify(item.value));
+          ctrl.enqueue("\r\n\r\n");
+        } catch (err) {
+          console.error(err);
           ctrl.close();
-          return;
+          controller.abort();
         }
+      },
 
-        ctrl.enqueue("data: ");
-        ctrl.enqueue(JSON.stringify(item.value));
-        ctrl.enqueue("\r\n\r\n");
+      async cancel() {
+        console.log("body.cancel");
+        controller.abort();
+
+        for await (const _ of output) {
+          // must be draining responses here to abort the backend llama request.
+        }
       }
     });
 
@@ -48,12 +78,20 @@ Deno.serve(async (req: Request) => {
       },
     );
   } else {
-    const output = await session.run(prompt, {
-      stream,
-      mode
-    });
+    try {
+      const output = await session.run(prompt, {
+        stream,
+        mode,
+        signal
+      });
 
-    console.log("done!");
-    return Response.json(output);
+      console.log("done");
+      return Response.json(output);
+    } catch (err) {
+      console.error(err);
+      return new Response(null, {
+        status: STATUS_CODE.InternalServerError
+      });
+    }
   }
 });

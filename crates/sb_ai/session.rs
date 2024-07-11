3cbabe4ffdd369370d827b54424eb3f980ca9639
kallebysantos <kalleby_santos@hotmail.com>
2024-07-11 12:39:20 +0100

    feat: add cuda provider support in session

--- crates/sb_ai/session.rs
+++ crates/sb_ai/session.rs
@@ -1,17 +1,28 @@
 use std::path::PathBuf;
 
 use anyhow::Error;
-use ort::{GraphOptimizationLevel, Session};
+use ort::{CUDAExecutionProvider, ExecutionProvider, GraphOptimizationLevel, Session};
 
 pub(crate) fn create_session(model_file_path: PathBuf) -> Result<Session, Error> {
     let orm_threads = std::env::var("OMP_NUM_THREADS")
         .map_or(None, |val| val.parse::<usize>().ok())
         .unwrap_or(1);
 
-    let session = Session::builder()?
+    let builder = Session::builder()?
         .with_optimization_level(GraphOptimizationLevel::Level3)?
-        .with_intra_threads(orm_threads)?
-        .commit_from_file(model_file_path)?;
+        .with_intra_threads(orm_threads)?;
 
+    let cuda = CUDAExecutionProvider::default();
+    if let Ok(is_cuda_available) = cuda.is_available() {
+        println!("ORT: CUDA is_available: [{}]", is_cuda_available);
+
+        if is_cuda_available {
+            if let Err(error) = cuda.register(&builder) {
+                eprintln!("ORT: Failed to register CUDA:  - {:?}", error);
+            }
+        }
+    }
+
+    let session = builder.commit_from_file(model_file_path)?;
     Ok(session)
 }

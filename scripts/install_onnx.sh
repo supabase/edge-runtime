595b93d496f86556a73873284f8892f0013e788d
kallebysantos <kalleby_santos@hotmail.com>
2024-07-11 12:40:59 +0100

    feat: improved `install_onnx` script
    `install_onnx` script now supports `--gpu` flag to download runtime with
    cuda provider

--- scripts/install_onnx.sh
+++ scripts/install_onnx.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-curl -O https://registry.npmjs.org/onnxruntime-node/-/onnxruntime-node-$1.tgz && tar zxvf onnxruntime-node-$1.tgz
+ONNX_VERSION=${1:-1.17.0}
+TARGETPLATFORM=$2
+SAVE_PATH=${3:-"./onnx-runtime"}
 
-if [ "$2" == "linux/arm64" ]; then
-  mv ./package/bin/napi-v3/linux/arm64/libonnxruntime.so.$1 $3
+ONNX_DOWNLOAD_FILE="onnxruntime-linux"
+ONNX_TARGET_PLATFORM=$([ "$TARGETPLATFORM" == "linux/arm64" ] && echo "aarch64" || echo "x64")
+
+if [[ $* == *"--gpu"* ]]; then
+	ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-gpu-$ONNX_VERSION"
 else
-  mv ./package/bin/napi-v3/linux/x64/libonnxruntime.so.$1 $3
+	ONNX_DOWNLOAD_FILE="$ONNX_DOWNLOAD_FILE-$ONNX_TARGET_PLATFORM-$ONNX_VERSION"
 fi
+
+wget -qO- "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/${ONNX_DOWNLOAD_FILE}.tgz" | tar zxv
+
+mv $ONNX_DOWNLOAD_FILE $SAVE_PATH

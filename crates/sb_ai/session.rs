5461b71fdf24eaef26cce0dcc05d781fdba7e0d0
kallebysantos <kalleby_santos@hotmail.com>
2024-07-14 10:05:19 +0100

    stamp: optimize pipeline loading
    - Using `HashMap` allows the reusing of pipeline sessions between
    requests.

--- crates/sb_ai/session.rs
+++ crates/sb_ai/session.rs
@@ -1,28 +1,46 @@
-use std::path::PathBuf;
+use once_cell::sync::Lazy;
+use std::{collections::HashMap, sync::Mutex};
+use std::{path::PathBuf, sync::Arc};
 
-use anyhow::Error;
+use anyhow::{anyhow, Error};
 use ort::{CUDAExecutionProvider, ExecutionProvider, GraphOptimizationLevel, Session};
 
-pub(crate) fn create_session(model_file_path: PathBuf) -> Result<Session, Error> {
-    let orm_threads = std::env::var("OMP_NUM_THREADS")
-        .map_or(None, |val| val.parse::<usize>().ok())
-        .unwrap_or(1);
+static SESSIONS: Lazy<Mutex<HashMap<String, Arc<Session>>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
 
-    let builder = Session::builder()?
-        .with_optimization_level(GraphOptimizationLevel::Level3)?
-        .with_intra_threads(orm_threads)?;
+pub(crate) fn create_session(model_file_path: PathBuf) -> Result<Arc<Session>, Error> {
+    let Some(model_path) = model_file_path.to_str() else {
+        return Err(anyhow!("failed to parse model filename as key"));
+    };
 
-    let cuda = CUDAExecutionProvider::default();
-    if let Ok(is_cuda_available) = cuda.is_available() {
-        println!("ORT: CUDA is_available: [{}]", is_cuda_available);
+    let mut sessions = SESSIONS.lock().unwrap();
 
-        if is_cuda_available {
-            if let Err(error) = cuda.register(&builder) {
-                eprintln!("ORT: Failed to register CUDA:  - {:?}", error);
+    let Some(session) = sessions.get(model_path) else {
+        println!("initializing a new session for {model_path}");
+        let orm_threads = std::env::var("OMP_NUM_THREADS")
+            .map_or(None, |val| val.parse::<usize>().ok())
+            .unwrap_or(1);
+
+        let builder = Session::builder()?
+            .with_optimization_level(GraphOptimizationLevel::Level3)?
+            .with_intra_threads(orm_threads)?;
+
+        let cuda = CUDAExecutionProvider::default();
+        if let Ok(is_cuda_available) = cuda.is_available() {
+            println!("ORT: CUDA is_available: [{}]", is_cuda_available);
+
+            if is_cuda_available {
+                if let Err(error) = cuda.register(&builder) {
+                    eprintln!("ORT: Failed to register CUDA:  - {:?}", error);
+                }
             }
         }
-    }
+        let session = builder.commit_from_file(&model_file_path).map(Arc::new)?;
+        let _ = &sessions.insert(model_path.to_string(), session.to_owned());
 
-    let session = builder.commit_from_file(model_file_path)?;
-    Ok(session)
+        return Ok(session);
+    };
+
+    println!("loading session for {model_path}");
+    Ok(session.to_owned())
 }

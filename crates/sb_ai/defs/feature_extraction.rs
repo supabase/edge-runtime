354245c8fa4a0e236b0f34319c7963e7f2166607
Nyannyacha <meow@nnc.gg>
2024-07-29 00:20:37 +0000

    refactor(sb_ai): reworked to be defining pipelines easily

---
+++ crates/sb_ai/defs/feature_extraction.rs
@@ -0,0 +1,212 @@
+use std::borrow::Cow;
+
+use anyhow::{anyhow, bail, Context, Error};
+use deno_core::error::AnyError;
+use deno_core::url::Url;
+use ndarray::{Array1, Axis, Ix3};
+use ndarray_linalg::norm::{normalize, NormalizeAxis};
+use ort::{inputs, Session};
+use serde::Deserialize;
+use tokenizers::Tokenizer;
+
+use crate::pipeline::{
+    try_get_model_url_from_env, try_get_tokenizer_url_from_env, PipelineDefinition,
+};
+
+use crate::tensor_ops::mean_pool;
+
+#[derive(Deserialize, Debug)]
+pub struct FeatureExtractionPipelineInput {
+    pub prompt: String,
+    pub mean_pool: bool,
+    pub normalize: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct FeatureExtractionPipeline;
+
+impl PipelineDefinition for FeatureExtractionPipeline {
+    type Input = FeatureExtractionPipelineInput;
+    type Output = Vec<f32>;
+
+    fn make() -> Self {
+        FeatureExtractionPipeline
+    }
+
+    fn name(&self) -> Cow<'static, str> {
+        "feature-extraction".into()
+    }
+
+    fn model_url(&self, _requested_variation: Option<&str>) -> Result<Url, AnyError> {
+        unimplemented!()
+    }
+
+    fn tokenizer_url(&self, _requested_variation: Option<&str>) -> Option<Result<Url, AnyError>> {
+        unimplemented!()
+    }
+
+    fn run(
+        &self,
+        session: &Session,
+        tokenizer: Option<&Tokenizer>,
+        input: &FeatureExtractionPipelineInput,
+    ) -> Result<<Self as PipelineDefinition>::Output, Error> {
+        let encoded_prompt = tokenizer
+            .unwrap()
+            .encode(input.prompt.to_owned(), true)
+            .map_err(anyhow::Error::msg)?;
+
+        let input_ids = encoded_prompt
+            .get_ids()
+            .iter()
+            .map(|i| *i as i64)
+            .collect::<Vec<_>>();
+
+        let attention_mask = encoded_prompt
+            .get_attention_mask()
+            .iter()
+            .map(|i| *i as i64)
+            .collect::<Vec<_>>();
+
+        let token_type_ids = encoded_prompt
+            .get_type_ids()
+            .iter()
+            .map(|i| *i as i64)
+            .collect::<Vec<_>>();
+
+        let input_ids_array = Array1::from_iter(input_ids.iter().cloned());
+        let input_ids_array = input_ids_array.view().insert_axis(Axis(0));
+
+        let attention_mask_array = Array1::from_iter(attention_mask.iter().cloned());
+        let attention_mask_array = attention_mask_array.view().insert_axis(Axis(0));
+
+        let token_type_ids_array = Array1::from_iter(token_type_ids.iter().cloned());
+        let token_type_ids_array = token_type_ids_array.view().insert_axis(Axis(0));
+
+        let outputs = session.run(inputs! {
+            "input_ids" => input_ids_array,
+            "token_type_ids" => token_type_ids_array,
+            "attention_mask" => attention_mask_array,
+        }?)?;
+
+        let embeddings = outputs["last_hidden_state"].try_extract_tensor()?;
+        let embeddings = embeddings.into_dimensionality::<Ix3>()?;
+
+        let result = if input.mean_pool {
+            mean_pool(embeddings, attention_mask_array.insert_axis(Axis(2)))
+        } else {
+            embeddings.into_owned().remove_axis(Axis(0))
+        };
+
+        let result = if input.normalize {
+            let (normalized, _) = normalize(result, NormalizeAxis::Row);
+            normalized
+        } else {
+            result
+        };
+
+        Ok(result.view().to_slice().unwrap().to_vec())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SupabaseGte {
+    inner: FeatureExtractionPipeline,
+}
+
+impl PipelineDefinition for SupabaseGte {
+    type Input = <FeatureExtractionPipeline as PipelineDefinition>::Input;
+    type Output = <FeatureExtractionPipeline as PipelineDefinition>::Output;
+
+    fn make() -> Self {
+        SupabaseGte {
+            inner: FeatureExtractionPipeline,
+        }
+    }
+
+    fn name(&self) -> Cow<'static, str> {
+        "supabase-gte".into()
+    }
+
+    fn model_url(&self, requested_variation: Option<&str>) -> Result<Url, AnyError> {
+        if let Some(Ok(url)) = try_get_model_url_from_env(self, requested_variation) {
+            return Ok(url);
+        }
+
+        Url::parse(include_str!("repo/supabase-gte/model.txt").trim())
+            .context("failed to parse URL")
+    }
+
+    fn tokenizer_url(&self, requested_variation: Option<&str>) -> Option<Result<Url, AnyError>> {
+        if let Some(Ok(url)) = try_get_tokenizer_url_from_env(self, requested_variation) {
+            return Some(Ok(url));
+        }
+
+        Some(
+            Url::parse(include_str!("repo/supabase-gte/tokenizer.txt").trim())
+                .context("failed to parse URL"),
+        )
+    }
+
+    fn configure_tokenizer(&self, tokenizer: &mut Tokenizer) {
+        self.inner.configure_tokenizer(tokenizer)
+    }
+
+    fn run(
+        &self,
+        session: &Session,
+        tokenizer: Option<&Tokenizer>,
+        input: &Self::Input,
+    ) -> Result<Self::Output, anyhow::Error> {
+        self.inner.run(session, tokenizer, input)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GteSmall {
+    inner: SupabaseGte,
+}
+
+impl PipelineDefinition for GteSmall {
+    type Input = <SupabaseGte as PipelineDefinition>::Input;
+    type Output = <SupabaseGte as PipelineDefinition>::Output;
+
+    fn make() -> Self {
+        GteSmall {
+            inner: SupabaseGte::make(),
+        }
+    }
+
+    fn name(&self) -> Cow<'static, str> {
+        "gte-small".into()
+    }
+
+    fn model_url(&self, requested_variation: Option<&str>) -> Result<Url, AnyError> {
+        if requested_variation.is_some() {
+            bail!("gte-small: model variation is not supported");
+        }
+
+        self.inner.model_url(requested_variation)
+    }
+
+    fn tokenizer_url(&self, requested_variation: Option<&str>) -> Option<Result<Url, AnyError>> {
+        if requested_variation.is_some() {
+            Some(anyhow!("gte-small: tokenizer variation is not supported"));
+        }
+
+        self.inner.tokenizer_url(requested_variation)
+    }
+
+    fn configure_tokenizer(&self, tokenizer: &mut Tokenizer) {
+        self.inner.configure_tokenizer(tokenizer)
+    }
+
+    fn run(
+        &self,
+        session: &Session,
+        tokenizer: Option<&Tokenizer>,
+        input: &Self::Input,
+    ) -> Result<Self::Output, AnyError> {
+        self.inner.run(session, tokenizer, input)
+    }
+}
---
+++ crates/sb_ai/defs/mod.rs
@@ -0,0 +1,58 @@
+use std::{borrow::Cow, cell::RefCell, collections::HashMap, rc::Rc, sync::Arc};
+
+use anyhow::bail;
+use deno_core::{error::AnyError, OpState};
+use futures::{future::LocalBoxFuture, FutureExt};
+use once_cell::sync::Lazy;
+
+use crate::pipeline::{self, PipelineDefinition};
+
+mod feature_extraction;
+
+type InitializerReturnType = LocalBoxFuture<'static, Result<(), AnyError>>;
+type Initializer = Arc<
+    dyn (Fn(Option<Rc<RefCell<OpState>>>, Option<String>) -> InitializerReturnType) + Send + Sync,
+>;
+
+fn factory<T>() -> (Cow<'static, str>, Initializer)
+where
+    T: PipelineDefinition + Clone + 'static,
+{
+    let def = T::make();
+    (def.name(), initializer_factory(def))
+}
+
+fn initializer_factory<T>(def: T) -> Initializer
+where
+    T: PipelineDefinition + Clone + 'static,
+{
+    Arc::new(move |state, variation| {
+        let def = def.clone();
+        let arg = if let Some(variation) = variation {
+            pipeline::PipelineInitArg::WithVariation { def, variation }
+        } else {
+            pipeline::PipelineInitArg::Simple(def)
+        };
+
+        pipeline::init(state, arg).boxed_local()
+    })
+}
+
+static DEFS: Lazy<HashMap<Cow<'static, str>, Initializer>> = Lazy::new(|| {
+    HashMap::from([
+        factory::<feature_extraction::SupabaseGte>(),
+        factory::<feature_extraction::GteSmall>(),
+    ])
+});
+
+pub async fn init(
+    state: Option<Rc<RefCell<OpState>>>,
+    name: &str,
+    variation: Option<String>,
+) -> Result<(), AnyError> {
+    if let Some(initalizer) = DEFS.get(name) {
+        (*initalizer)(state, variation).await
+    } else {
+        bail!("unsupported pipeline: {name}");
+    }
+}

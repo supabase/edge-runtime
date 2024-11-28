c2759e0787727c7f80bd983bd5eb978e75089021
Nyannyacha <meow@nnc.gg>
2024-11-28 22:06:15 +0000

    chore: fill global definitions as far as possible

--- crates/sb_core/js/main_worker.js
+++ crates/sb_core/js/main_worker.js
@@ -1,11 +1,13 @@
+import { core, primordials } from 'ext:core/mod.js';
+
 import { MAIN_WORKER_API as ai } from 'ext:sb_ai/js/ai.js';
 import { SUPABASE_USER_WORKERS } from 'ext:sb_user_workers/user_workers.js';
 import { applySupabaseTag } from 'ext:sb_core_main_js/js/http.js';
-import { core } from 'ext:core/mod.js';
 
 const ops = core.ops;
+const { ObjectDefineProperty } = primordials;
 
-Object.defineProperty(globalThis, 'EdgeRuntime', {
+ObjectDefineProperty(globalThis, 'EdgeRuntime', {
 	get() {
 		return {
 			ai,

4576c097eb7297f3a9410e2aec886829c2a8a188
Nyannyacha <meow@nnc.gg>
2024-12-17 02:44:47 +0000

    stamp: polishing

--- examples/main/index.ts
+++ examples/main/index.ts
@@ -103,7 +103,7 @@ Deno.serve(async (req: Request) => {
 
     const createWorker = async () => {
         const memoryLimitMb = 150;
-        const workerTimeoutMs = 100 * 1000;
+        const workerTimeoutMs = 5 * 60 * 1000;
         const noModuleCache = false;
 
         // you can provide an import map inline
@@ -129,8 +129,8 @@ Deno.serve(async (req: Request) => {
         // or load module source from an inline module
         // const maybeModuleCode = 'Deno.serve((req) => new Response("Hello from Module Code"));';
         //
-        const cpuTimeSoftLimitMs = 1000;
-        const cpuTimeHardLimitMs = 2000;
+        const cpuTimeSoftLimitMs = 10000;
+        const cpuTimeHardLimitMs = 20000;
 
         return await EdgeRuntime.userWorkers.create({
             servicePath,

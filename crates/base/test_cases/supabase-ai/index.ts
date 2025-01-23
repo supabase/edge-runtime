0ac8fe9f0a512ed78f1430c2136820f3ce4419df
kallebysantos <kalleby_santos@hotmail.com>
2025-01-23 19:36:14 +0000

    test(sb_ai): ensuring `mean_pool` and `normalize`
    - Applying `dot product` between two embeddings to ensures that
    `mean_pool` and `normalize` is working properly

--- crates/base/test_cases/supabase-ai/index.ts
+++ crates/base/test_cases/supabase-ai/index.ts
@@ -1,18 +1,45 @@
+import { assertGreater, assertLessOrEqual } from 'jsr:@std/assert';
+
 const session = new Supabase.ai.Session('gte-small');
 
-export default {
-    async fetch() {
-        // Generate embedding
-        const embedding = await session.run("meow", {
-            mean_pool: true,
-            normalize: true
-        });
+function dotProduct(a: number[], b: number[]) {
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
 
-        return new Response(
-            null,
-            {
-                status: embedding instanceof Array ? 200 : 500
-            }
-        );
-    }
+  return result;
 }
+
+export default {
+  async fetch() {
+    // Generate embedding
+    const meow = await session.run('meow', {
+      mean_pool: true,
+      normalize: true,
+    });
+
+    const love = await session.run('I love cats', {
+      mean_pool: true,
+      normalize: true,
+    });
+
+    // Ensures `mean_pool` and `normalize`
+    const sameScore = dotProduct(meow, meow);
+    const diffScore = dotProduct(meow, love);
+
+    assertGreater(sameScore, 0.9);
+    assertGreater(diffScore, 0.5);
+    assertGreater(sameScore, diffScore);
+
+    assertLessOrEqual(sameScore, 1);
+    assertLessOrEqual(diffScore, 1);
+
+    return new Response(
+      null,
+      {
+        status: meow instanceof Array && meow.length == 384 ? 200 : 500,
+      },
+    );
+  },
+};

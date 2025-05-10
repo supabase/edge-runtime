f311d25f1a08dd4533ee232631085a17b670f8d2
kallebysantos <kalleby_santos@hotmail.com>
2025-05-10 19:55:08 +0100

    stamp: moving streaming utils to `llm` folder
    - Extracting json parsers to a separated file
    - Moving LLM stream related code to a separated folder

---
+++ ext/ai/js/llm/utils/json_parser.ts
@@ -0,0 +1,82 @@
+import EventSourceStream from './event_source_stream.mjs';
+
+// Adapted from https://github.com/ollama/ollama-js/blob/6a4bfe3ab033f611639dfe4249bdd6b9b19c7256/src/utils.ts#L262
+// TODO:(kallebysantos) need to simplify it
+export async function* parseJSON<T extends object>(
+  itr: ReadableStream<Uint8Array>,
+  signal: AbortSignal,
+) {
+  let buffer = '';
+
+  const decoder = new TextDecoder('utf-8');
+  const reader = itr.getReader();
+
+  while (true) {
+    try {
+      if (signal.aborted) {
+        reader.cancel(signal.reason);
+        reader.releaseLock();
+        return { error: signal.reason };
+      }
+
+      const { done, value } = await reader.read();
+
+      if (done) {
+        break;
+      }
+
+      buffer += decoder.decode(value);
+
+      const parts = buffer.split('\n');
+
+      buffer = parts.pop() ?? '';
+
+      for (const part of parts) {
+        yield JSON.parse(part) as T;
+      }
+    } catch (error) {
+      yield { error };
+    }
+  }
+
+  for (const part of buffer.split('\n').filter((p) => p !== '')) {
+    try {
+      yield JSON.parse(part) as T;
+    } catch (error) {
+      yield { error };
+    }
+  }
+}
+
+// TODO:(kallebysantos) need to simplify it
+export async function* parseJSONOverEventStream(
+  itr: ReadableStream<Uint8Array>,
+  signal: AbortSignal,
+) {
+  const decoder = new EventSourceStream();
+
+  itr.pipeThrough(decoder);
+
+  const reader: ReadableStreamDefaultReader<MessageEvent> = decoder.readable
+    .getReader();
+
+  while (true) {
+    try {
+      if (signal.aborted) {
+        reader.cancel(signal.reason);
+        reader.releaseLock();
+        return { error: signal.reason };
+      }
+
+      const { done, value } = await reader.read();
+
+      if (done) {
+        break;
+      }
+
+      yield JSON.parse(value.data);
+    } catch (error) {
+      yield { error };
+    }
+  }
+}

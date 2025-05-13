98ad9f1a1ab37aa7733729a5eb6e904379263d30
kallebysantos <kalleby_santos@hotmail.com>
2025-05-13 18:11:55 +0100

    fix: openai streaming
    - OpenAI uses a different streaming alternative that ends with `[DONE]`

--- ext/ai/js/llm/utils/event_source_stream.mjs
+++ ext/ai/js/llm/utils/event_source_stream.mjs
@@ -1,4 +1,4 @@
-import EventStreamParser from './event_stream_parser.mjs';
+import EventStreamParser from "./event_stream_parser.mjs";
 /**
  * A Web stream which handles Server-Sent Events from a binary ReadableStream like you get from the fetch API.
  * Implements the TransformStream interface, and can be used with the Streams API as such.
@@ -9,11 +9,19 @@ class EventSourceStream {
     // 1. The SSE spec allows for an optional UTF-8 BOM.
     // 2. We have to use a *streaming* decoder, in case two adjacent data chunks are split up in the middle of a
     // multibyte Unicode character. Trying to parse the two separately would result in data corruption.
-    const decoder = new TextDecoderStream('utf-8');
+    const decoder = new TextDecoderStream("utf-8");
     let parser;
     const sseStream = new TransformStream({
       start(controller) {
         parser = new EventStreamParser((data, eventType, lastEventId) => {
+          // NOTE:(kallebysantos) Some providers like OpenAI send '[DONE]'
+          // to indicates stream terminates, so we need to check if the SSE contains "[DONE]" and close the stream
+          if (typeof data === "string" && data.trim() === "[DONE]") {
+            controller.terminate?.(); // If supported
+            controller.close?.(); // Fallback
+            return;
+          }
+
           controller.enqueue(
             new MessageEvent(eventType, { data, lastEventId }),
           );

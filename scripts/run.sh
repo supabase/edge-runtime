3e4ecef1f94631ea3e1aa32d3b3336bdd9a26d07
gofenix <codeless98@163.com>
2025-09-21 00:00:31 +0800

    fix: use absolute paths for main service and event worker locations

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT_SOURCE="${BASH_SOURCE[0]:-$0}"
+while [ -h "$SCRIPT_SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SCRIPT_SOURCE")" >/dev/null 2>&1 && pwd)"
+  SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+  [[ "$SCRIPT_SOURCE" != /* ]] && SCRIPT_SOURCE="$DIR/$SCRIPT_SOURCE"
+done
+SCRIPTPATH="$(cd -P "$(dirname "$SCRIPT_SOURCE")" >/dev/null 2>&1 && pwd)"
 
 export $(grep -v '^#' $SCRIPTPATH/../.env | xargs)
 
 # --features cli/tracing
 cargo build --features cli/tracing && \
 RUST_BACKTRACE=full ./target/debug/edge-runtime "$@" start \
-    --main-service ./examples/main \
-    --event-worker ./examples/event-manager
+    --main-service "$SCRIPTPATH/../examples/main" \
+    --event-worker "$SCRIPTPATH/../examples/event-manager"

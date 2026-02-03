eecbf117fc63a55610fa8ad00f36314ef2c70c34
냥냐챠 <meow@nnc.gg>
2026-02-03 18:31:49 +0900

    fix: adjust bootstrap script (#658)
    * fix: `process.std*` are not exposed in main worker
    
    * fix: expose `MessagePort`

--- ext/runtime/js/bootstrap.js
+++ ext/runtime/js/bootstrap.js
@@ -263,6 +263,7 @@ const globalScope = {
   performance: writable(performance.performance),
 
   // messagePort
+  MessagePort: nonEnumerable(messagePort.MessagePort),
   MessageChannel: nonEnumerable(messagePort.MessageChannel),
   structuredClone: writable(messagePort.structuredClone),
 
@@ -768,7 +769,6 @@ globalThis.bootstrapSBEdge = (opts, ctx) => {
     });
   }
 
-  const nodeBootstrap = globalThis.nodeBootstrap;
   if (nodeBootstrap) {
     nodeBootstrap({
       runningOnMainThread: true,
@@ -783,6 +783,8 @@ globalThis.bootstrapSBEdge = (opts, ctx) => {
   delete globalThis.bootstrapSBEdge;
 };
 
+const nodeBootstrap = globalThis.nodeBootstrap;
+
 globalThis.bootstrap = {
   dispatchLoadEvent,
   dispatchUnloadEvent,
@@ -794,3 +796,4 @@ globalThis.bootstrap = {
 
 core.setUnhandledPromiseRejectionHandler(processUnhandledPromiseRejection);
 core.setHandledPromiseRejectionHandler(processRejectionHandled);
+nodeBootstrap({ warmup: true });

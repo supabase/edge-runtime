7f1d557ccb7f269b1031ceb8bee44f4edaf60b30
kallebysantos <kalleby_santos@hotmail.com>
2026-04-11 13:55:09 +0100

    stamp: creating wasm-modules example

---
+++ examples/wasm-module/add-wasm/pkg/add_wasm.js
@@ -0,0 +1,35 @@
+/* @ts-self-types="./add_wasm.d.ts" */
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export function add(a, b) {
+    const ret = wasm.add(a, b);
+    return ret >>> 0;
+}
+
+function __wbg_get_imports() {
+    const import0 = {
+        __proto__: null,
+        __wbindgen_init_externref_table: function() {
+            const table = wasm.__wbindgen_externrefs;
+            const offset = table.grow(4);
+            table.set(0, undefined);
+            table.set(offset + 0, undefined);
+            table.set(offset + 1, null);
+            table.set(offset + 2, true);
+            table.set(offset + 3, false);
+        },
+    };
+    return {
+        __proto__: null,
+        "./add_wasm_bg.js": import0,
+    };
+}
+
+const wasmUrl = new URL('add_wasm_bg.wasm', import.meta.url);
+const wasmInstantiated = await WebAssembly.instantiateStreaming(fetch(wasmUrl), __wbg_get_imports());
+const wasm = wasmInstantiated.instance.exports;
+wasm.__wbindgen_start();
---
+++ examples/wasm-module/add-wasm/pkg/add_wasm_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const add: (a: number, b: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
